a7e051f110f37804857d995271c8369af6a91337
Tim Officer <timothyofficer@gmail.com>
2015-05-08 11:29:53 -0400

    home page styles

--- src/app/components/home/homeCtrl.js
+++ src/app/components/home/homeCtrl.js
@@ -1,12 +1,16 @@
 angular.module('throughCompanyApp').controller('homeCtrl', [
   '$scope',
+  '$state',
   '$rootScope',
   'projectService',
   '$timeout',
   'subscribeService',
   'utilsService',
   'skillService',
-  function($scope, $rootScope, projectService, $timeout, subscribeService, utilsService, skillService) {
+  'userService',
+  'authService',
+  'routes',
+  function($scope, $state, $rootScope, projectService, $timeout, subscribeService, utilsService, skillService, userService, authService, routes) {
     $scope.loaded = false;
 
     projectService.getProjects({}).then(function success(response) {
@@ -19,6 +23,21 @@ angular.module('throughCompanyApp').controller('homeCtrl', [
     });
 
     // ---------------- buttons ----------------
+    $scope.registerSubmitting = null;
+    $scope.registerResult = null;
+    $scope.registerBtnOptions = {
+      buttonDefaultText: 'Sign up',
+      buttonDefaultClass: 'btn btn-primary',
+      buttonSubmittingText: 'Signing up...',
+      buttonSubmittingIcon: 'icon-left fa fa-spin fa-refresh',
+      buttonSuccessText: 'Sign up successful',
+      buttonSuccessIcon: 'icon-left fa fa-check',
+      buttonSuccessClass: 'btn-success',
+      buttonErrorText: 'Error signing up',
+      buttonErrorIcon: 'icon-left fa fa-remove',
+      buttonErrorClass: 'btn-danger'
+    };
+
     // subscribe button
     $scope.subscribeSubmitting = null;
     $scope.subscribeResult = null;
@@ -69,5 +88,58 @@ angular.module('throughCompanyApp').controller('homeCtrl', [
     };
 
     $scope.form = {};
+
+    // ---------------- Register ---------------- //
+    $scope.form = {};
+    $scope.errorMsg = null;
+
+    $scope.register = function(registerForm) {
+
+      $scope.submitted = true;
+
+      if (!registerForm.$valid) {
+        $timeout(function() {
+          $scope.submitted = false;
+          $scope.registerForm.$setPristine();
+        }, 2000);
+        return;
+      };
+
+      $scope.registerSubmitting = true;
+
+      $timeout(function() {
+        userService.create({
+          email: $scope.form.email,
+          password: $scope.form.password
+        }).then(function success(response) {
+          authService.login($scope.form.email, $scope.form.password)
+            .then(function success(response) {
+              $scope.registerSubmitting = false;
+              $rootScope.currentUser = response.user;
+              authService.getUserClaims();
+
+              $scope.loginResult = 'success';
+
+              $state.go(routes.user, {
+                userName: $scope.currentUser.userName
+              });
+            }, function error(response) {
+              $scope.registerSubmitting = false;
+              $scope.registerResult = 'error';
+              $scope.errorMsg = 'Invalid email or password';
+              $timeout(function() {
+                $scope.errorMsg = null;
+              }, 3000);
+            });
+        }, function error(response) {
+          $scope.registerSubmitting = false;
+          $scope.registerResult = 'error';
+          $scope.errorMsg = utilsService.getServerErrorMessage(response);
+          $timeout(function() {
+            $scope.errorMsg = null;
+          }, 3000);
+        });
+      }, 500);
+    };
   }
 ]);
