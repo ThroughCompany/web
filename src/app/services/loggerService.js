cdde8ff083837f77a38f1a63c5c8d5d7a39cf329
Tim Officer <timothyofficer@gmail.com>
2015-05-14 10:50:16 -0400

    roller errors

--- src/app/services/loggerService.js
+++ src/app/services/loggerService.js
@@ -1,26 +1,39 @@
 angular.module('throughCompanyApp').factory('loggerService', [
   'appSettings',
   function(appSettings) {
-
     function Logger() {}
 
+    var log = appSettings.ENV === 'production' || appSettings.ENV === 'development';
+
     Logger.prototype.debug = function debug(message) {
-      if (appSettings.ENV === 'production') return;
+      if (log) {
+        Rollbar.debug(message);
+        return;
+      }
       console.debug(message);
     };
 
     Logger.prototype.error = function error(message) {
-      if (appSettings.ENV === 'production') return;
+      if (log) {
+        Rollbar.error(message);
+        return;
+      }
       console.error(message);
     };
 
     Logger.prototype.info = function info(message) {
-      if (appSettings.ENV === 'production') return;
+      if (log) {
+        Rollbar.info(message);
+        return;
+      }
       console.info(message);
     };
 
     Logger.prototype.warning = function warning(message) {
-      if (appSettings.ENV === 'production') return;
+      if (log) {
+        Rollbar.warning(message);
+        return;
+      }
       console.warn(message);
     };
 
